31dbbc5834d633f9e5b98a8b3c02f6372a227de9
Min Seok Kim <minseok.kim@brain.snu.ac.kr>
2023-10-30 10:46:37 +0900

    Build: Change python minimum version.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/minsmis/ANTS/issues",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
